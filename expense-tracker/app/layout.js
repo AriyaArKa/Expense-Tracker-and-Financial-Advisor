e43863e504a3429a766d7269cfc0bc36b7553065
Abdullah Md. ShahPoran <abdullahshahporan@gmail.com>
2025-10-10 20:36:16 +0600

    Added Sonner

--- expense-tracker/app/layout.js
+++ expense-tracker/app/layout.js
@@ -1,27 +1,31 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header";
+import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Create Next App",
-  description: "one stop finance platform",
+  title: "Welth",
+  description: "One stop Finance Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className}`} suppressHydrationWarning={true}>
-          {/* header */}
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
-          {/* footer */}
+          <Toaster richColors />
+
           <footer className="bg-blue-50 py-12">
-            <div className="container mx-auto text-center text-gray-600 px-4">
-              <p>© 2025 Expense Tracker. All rights reserved.</p>
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by RoadsideCoder</p>
             </div>
           </footer>
         </body>
